8a6703458f4251b8223838eedb15b2712aba2c5c
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-02-21 17:27:49 +0530

    Create 5.c

---
+++ Week 4/Lab Excercises/5.c
@@ -0,0 +1,17 @@
+// In this challenge you will print a shape on the screen using asterisks (“*”) characters. The user will be prompted to enter an ODD number between 1 and 99. The shape that will be printed resembles a diamond, where the number provided by the user represents the number of rows.
+
+// Input
+
+// 3
+
+// Output
+
+//   *
+
+//  ***
+
+// *****
+
+//  ***
+
+//   *
