4a0cc49f6b961008ed77ab10f5b3b002fb2c0766
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-02-21 17:28:04 +0530

    Create 6.c

---
+++ Week 4/Lab Excercises/6.c
@@ -0,0 +1,45 @@
+// Karan is the host at a spelling bee contest. He pronounces words to the contestants and they have to spell the correct word. So to save himself some time, he asks you to write a program in which he saves the correct spellings and the contestants enter their answer into the computer.
+
+// The degree of correctness is as follows:
+
+// TRUE if it is an exact match, ALMOST THERE if no more than 2 letters are wrong, FALSE if 3 or more letters are wrong. There are T test cases.
+
+// Input Format
+
+// The first line of the input is an integer that represents the number of test cases. T test cases follow. First line of each test case contains the correct answer and second line contains the contestant’s answer.
+
+// Output Format
+
+// For each test case output the degree of correctness. All letters are upper case. The output is to be formatted exactly like that for the sample output given below.
+
+// Sample Input
+
+// 3
+
+// SAMPLE
+
+// SIMPLE
+
+// THEIR
+
+// THEIR
+
+// WINDMILL
+
+// WINDOWS
+
+// Sample Output
+
+// ALMOST THERE
+
+// TRUE
+
+// FALSE
+
+// Explanation
+
+// SIMPLE has 1 letter wrong, ALMOST THERE
+
+// THEIR is exactly same, TRUE
+
+// WINDOWS has more than 2 letters wrong, FALSE
