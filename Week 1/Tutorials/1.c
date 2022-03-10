69f2619d0ec83d7cc583a9d343bf2afef09c4694
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-03-10 11:24:16 +0530

    Create 1.c

---
+++ Week 1/Tutorials/1.c
@@ -0,0 +1,25 @@
+// The student strength at your college has crossed the 1000 mark now. The small stationary shop opposite to your college is not able to cater to the growing stationary needs of your college student community. The students have to always wait in a long queue to purchase stationary items. The shop-keeper is very weak in Mathematics and he always takes long time to do even simple addition. The shop-keeper's poor mathematical skills annoyed your college students as they had to wait for a long time in the queue. The current first year batch of your college have started learning C programming and they decided to help the shop-keeper by writing a program to add two numbers.
+
+// Input Format: Input consists of 2 integers.
+
+// Output Format: Refer sample input and output for formatting specifications.
+
+// Sample Input-1
+
+// 43
+
+// 53
+
+// Sample Output-1
+
+// 96
+
+#include <stdio.h>
+int main(){
+    int a,b,sum;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
